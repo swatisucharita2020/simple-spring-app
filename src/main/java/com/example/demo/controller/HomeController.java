6fb3a3074ec7c1e12f686633940f3ea8d5120ea6
swatisucharita2020 <68230595+swatisucharita2020@users.noreply.github.com>
2022-02-01 21:54:41 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service and load testing 1";
+        return "Hello from Azure App Service and load testing 2";
     }
 }
